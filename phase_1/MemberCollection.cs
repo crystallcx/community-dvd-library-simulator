ee68c39e50bf858d6e48c5c6fd6663fb52663fd4
crystallcx <crystaltienlee@gmail.com>
2022-08-21 19:08:55 +1000

    add code + specification from phase 1 and phase 2 of project

---
+++ phase_1/MemberCollection.cs
@@ -0,0 +1,192 @@
+﻿//CAB301 assessment 1 - 2022
+//The implementation of MemberCollection ADT
+
+class MemberCollection : IMemberCollection
+{
+    // Fields
+    private int capacity;
+    private int count;
+    private Member[] members; //make sure members are sorted in dictionary order
+
+    // Properties
+
+    // get the capacity of this member colllection 
+    // pre-condition: nil
+    // post-condition: return the capacity of this member collection and this member collection remains unchanged
+    public int Capacity { get { return capacity; } }
+
+    // get the number of members in this member colllection 
+    // pre-condition: nil
+    // post-condition: return the number of members in this member collection and this member collection remains unchanged
+    public int Number { get { return count; } }
+
+
+
+
+    // Constructor - to create an object of member collection 
+    // Pre-condition: capacity > 0
+    // Post-condition: an object of this member collection class is created
+
+    public MemberCollection(int capacity)
+    {
+        if (capacity > 0)
+        {
+            this.capacity = capacity;
+            members = new Member[capacity];
+            count = 0;
+        }
+    }
+
+    // check if this member collection is full
+    // Pre-condition: nil
+    // Post-condition: return ture if this member collection is full; otherwise return false.
+    public bool IsFull()
+    {
+        return count == capacity;
+    }
+
+    // check if this member collection is empty
+    // Pre-condition: nil
+    // Post-condition: return ture if this member collection is empty; otherwise return false.
+    public bool IsEmpty()
+    {
+        return count == 0;
+    }
+
+    // Add a new member to this member collection
+    // Pre-condition: this member collection is not full
+    // Post-condition: a new member is added to the member collection and the members are sorted in ascending order by their full names;
+    // No duplicate will be added into this the member collection
+    public void Add(IMember member)
+    {
+        //if member collection full, terminate
+        if (IsFull())
+        {
+            Console.WriteLine("Member collection full.");
+            return;
+        }
+
+        //quickly check (using binary search) if member already exists, if so, terminate
+        if (Search(member) == true)
+        {
+            Console.WriteLine("Member cannot be added, already exists in collection.");
+            return;
+        }
+
+        //add non-duplicate member into array
+        int i;
+        for (i = count - 1; (i >= 0 && member.CompareTo(members[i]) < 1); i--)
+        {
+            members[i + 1] = members[i];
+        }
+
+        members[i + 1] = (Member)member; 
+        count++; //increase member count
+    }
+
+    // Remove a given member out of this member collection
+    // Pre-condition: nil
+    // Post-condition: the given member has been removed from this member collection, if the given meber was in the member collection
+    public void Delete(IMember aMember)
+    {
+        //if colleciton is empty, terminate 
+        if (IsEmpty())
+        {
+            Console.WriteLine("Collection empty. No members to delete.");
+            return;
+        }
+
+        //initialise variables
+        int lower = 0;
+        int upper = count - 1;
+        int mid;
+        bool found = false;
+
+        //find index of member to be deleted from array
+        while (lower <= upper)
+        {
+            mid = (lower + upper) / 2;
+            if (aMember.CompareTo(members[mid]) == 0)
+            {
+                lower = mid;
+                found = true;
+                break;
+            }
+            else if (aMember.CompareTo(members[mid]) == -1)
+                upper = mid - 1;
+
+            else
+                lower = mid + 1;
+        }
+
+        //if member does not exist in array, terminate
+        if (found == false)
+        {
+            Console.WriteLine($"Member does not exist in collection.");
+            return;
+        }
+
+        //delete element from array
+        for (int i = lower; i < count - 1; i++)
+            members[i] = members[i + 1];
+
+        count--; //decrease member count
+        members[count] = null; 
+        Console.WriteLine($"Member successfully deleted.");
+    }
+
+    // Search a given member in this member collection 
+    // Pre-condition: nil
+    // Post-condition: return true if this memeber is in the member collection; return false otherwise; member collection remains unchanged
+    public bool Search(IMember member)
+    {
+        if (IsEmpty())
+            return false;
+
+        //initialise variables
+        int lower = 0;
+        int upper = count - 1;
+        int mid;
+
+        while (lower <= upper)
+        {
+            mid = (lower + upper) / 2;
+            if (member.CompareTo(members[mid]) == 0)
+            {
+               // Console.WriteLine("Member exists in collection.");
+                return true;
+            }
+            else if (member.CompareTo(members[mid]) == -1)
+                upper = mid - 1;
+            else
+                lower = mid + 1;
+        }
+        //Console.WriteLine($"Member {member.FirstName}, {member.FirstName} does not exist in collection.");
+        return false;
+    }
+
+    // Remove all the members in this member collection
+    // Pre-condition: nil
+    // Post-condition: no member in this member collection 
+    public void Clear()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            this.members[i] = null;
+        }
+        count = 0;
+    }
+
+    // Return a string containing the information about all the members in this member collection.
+    // The information includes last name, first name and contact number in this order
+    // Pre-condition: nil
+    // Post-condition: a string containing the information about all the members in this member collection is returned
+    public string ToString()
+    {
+        string s = "";
+        for (int i = 0; i < count; i++)
+            s = s + members[i].ToString() + "\n";
+        return s;
+    }
+}
+
---
+++ phase_2/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace CAB301Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Member member1 = new Member("Aone", "Onelast", "0404444444", "4837"); //3
+            Member member2 = new Member("Btwo", "Twolast", "0404444444", "4837"); //5
+            Member member3 = new Member("Cthree", "Threelast", "0404444444", "4837"); //4
+            Member member4 = new Member("Dfour", "Fourlast", "0404444444", "4837"); //2
+            Member member5 = new Member("five", "Fivelast", "0404444444", "4837"); //1
+            Member member6 = new Member("Dsix", "Sixlast", "0404444444", "4837"); //4            Member member4 = new Member("Dfour", "Fourlast", "0404444444", "4837"); //2
+            Member member7 = new Member("ad", "Fivelast", "0404444444", "4837"); //1
+            Member member8 = new Member("fidave", "Fivelast", "0404444444", "4837"); //1
+            Member member9 = new Member("fivsse", "Fivelast", "0404444444", "4837"); //1
+            Member member10 = new Member("fasive", "Fivelast", "0404444444", "4837"); //1
+            Member member11 = new Member("Dsssix", "Sixlast", "0404444444", "4837"); //4
+
+            Movie movie9 = new Movie("Star Wars", MovieGenre.Action, MovieClassification.G, 60, 12);
+            Movie movie10 = new Movie("My Mister", MovieGenre.Action, MovieClassification.G, 60, 3);
+            Movie movie11 = new Movie("Scarlet Heart Ryeo", MovieGenre.Action, MovieClassification.G, 60, 3);
+
+            movie9.ToString();
+            movie9.AddBorrower(member1);
+            movie9.AddBorrower(member2);
+            movie9.AddBorrower(member3);
+            movie9.AddBorrower(member4);
+            movie9.AddBorrower(member5);
+            movie9.AddBorrower(member6);            
+            movie9.AddBorrower(member7);
+            movie9.AddBorrower(member8);
+            movie9.AddBorrower(member9);
+            movie9.AddBorrower(member10);
+            movie9.AddBorrower(member11);
+            Console.WriteLine(movie9.Borrowers.ToString());
+            movie9.ToString();
+
+            movie9.RemoveBorrower(member5);
+            Console.WriteLine(movie9.Borrowers.ToString());
+            movie9.ToString();
+
+            //create movie objects with only title name (for now)
+            Movie movie1 = new Movie("Revenge of the Sith", MovieGenre.Action, MovieClassification.G, 65, 3);
+            Movie movie2 = new Movie("Die Hard", MovieGenre.Action, MovieClassification.G, 65, 3);
+            Movie movie3 = new Movie("Willy Wonker", MovieGenre.Action, MovieClassification.G, 65, 3);
+            Movie movie4 = new Movie("Lord of the rings", MovieGenre.Action, MovieClassification.G, 65, 3);
+            Movie movie5 = new Movie("The Clone Wars", MovieGenre.Action, MovieClassification.G, 65, 3);
+            Movie movie6 = new Movie("Plup Fiction", MovieGenre.Action, MovieClassification.G, 65, 3);
+            Movie movie7 = new Movie("Casino Royal", MovieGenre.Action, MovieClassification.G, 65, 3);
+            Movie movie8 = new Movie("The Wizard of Oz", MovieGenre.Action, MovieClassification.G, 65, 3);
+
+            //create a new Bianry Tree Search - MovieCollection
+            MovieCollection newMovieCollections = new MovieCollection();
+
+            //Insert 8 members, try add a duplicate movie title 
+            //Console.WriteLine($"Inserting movie: {movie1.Title} == {newMovieCollections.Insert(movie1)}");
+            //Console.WriteLine($"Inserting movie: {movie2.Title} == {newMovieCollections.Insert(movie2)}");
+            //Console.WriteLine($"Inserting movie: {movie3.Title} == {newMovieCollections.Insert(movie3)}");
+            //Console.WriteLine($"Inserting movie: {movie4.Title} == {newMovieCollections.Insert(movie4)}");
+            //Console.WriteLine($"Inserting movie: {movie5.Title} == {newMovieCollections.Insert(movie5)}");
+            //Console.WriteLine($"Inserting movie: {movie6.Title} == {newMovieCollections.Insert(movie6)}");
+            //Console.WriteLine($"Inserting movie: {movie5.Title} == {newMovieCollections.Insert(movie5)}");
+            //Console.WriteLine($"Inserting movie: {movie7.Title} == {newMovieCollections.Insert(movie7)}");
+            //Console.WriteLine($"Inserting movie: {movie8.Title} == {newMovieCollections.Insert(movie8)}");
+            //Console.WriteLine($"Inserting movie: {movie9.Title} == {newMovieCollections.Insert(movie9)}");
+            //Console.WriteLine($"Inserting movie: {movie10.Title} == {newMovieCollections.Insert(movie10)}");
+            //Console.WriteLine($"Inserting movie: {movie11.Title} == {newMovieCollections.Insert(movie11)}");
+ 
+            Console.WriteLine();
+
+            IMovie[] testArray = newMovieCollections.ToArray();
+               foreach (IMovie movie in testArray)
+               {
+                   if (movie != null)
+                   {
+                       Console.WriteLine($"Movies in To Array = {movie.Title}");
+                   }
+               }
+
+            //Console.WriteLine("Test Search");
+            //IMovie test = newMovieCollections.Search("my Mister");
+            //Console.WriteLine(test.ToString());
+
+            //EXPECTED RESULTS
+            // Casino Royal
+            // Die Hard
+            // Lord of the rings
+            // Pulp Fiction
+            // Revenge of the Sith
+            // The Clone Wars
+            // The Wizard of Oz
+            // Willy Wonker
+
+
+
+        }
+    }
+}
+
+
+
+
+
+
